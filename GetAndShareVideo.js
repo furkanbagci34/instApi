65462ee3ffe5e962e6babb44f47186aa62cbdecc
furkanbagci34 <62967565+furkanbagci34@users.noreply.github.com>
2021-07-27 23:16:43 +0300

    fix id

--- GetAndShareVideo.js
+++ GetAndShareVideo.js
@@ -19,6 +19,7 @@ let video3
 let videocaption1
 let videocaption2
 let videocaption3
+let repositoryId
 
 const client = new Instagram({ username, password })
 
@@ -28,7 +29,7 @@ function LoginAndStart()
 {
   console.log(1)
   ;(async () => {
-    await client.login({ username: 'canlimaclinkleri3', password: '5795612f' })       
+    await client.login({ username: 'kisahikayetv', password: '5795612f' })       
 
     setInterval(OtoShare, 500000);
     //setInterval(OtoFollow,7200000)
@@ -44,30 +45,29 @@ function OtoShare()
       // const Post1 = await client.getPhotosByUsername({ username: 'sporx', first: '1', after: ''  })
       // const Post2 = await client.getPhotosByUsername({ username: 'sporotag', first: '1', after: ''  })
       // const Post3 = await client.getPhotosByUsername({ username: 'bbofootball', first: '1', after: ''  })
-      const Post4 = await client.getPhotosByUsername({ username: 'football_videos_status_', first: '1', after: ''  })
+      const Post4 = await client.getPhotosByUsername({ username: 'tv_makara', first: '1', after: ''  })
       // const Post5 = await client.getPhotosByUsername({ username: 'football_video_status', first: '1', after: ''  })
       // const Post6 = await client.getPhotosByUsername({ username: 'football___editors___video', first: '1', after: ''  })
   
-     if(Post4.user.edge_owner_to_timeline_media.edges[0].node.__typename == 'GraphVideo' )
-     {
-       console.log(resim4)
-       console.log(Post4.user.edge_owner_to_timeline_media.edges[0].node.thumbnail_src)
-       if(Post4.user.edge_owner_to_timeline_media.edges[0].node.thumbnail_src != resim4)
-       {
-          resim4 = Post4.user.edge_owner_to_timeline_media.edges[0].node.thumbnail_src
-          video1 = Post4.user.edge_owner_to_timeline_media.edges[0].node.video_url
-          console.log(resim4)
-          videocaption1 = Post4.user.edge_owner_to_timeline_media.edges[0].node.edge_media_to_caption.edges[0].node.text
-          Downloadvideo1()
-       }
-     }
+    if(Post4.user.edge_owner_to_timeline_media.edges[0].node.__typename == 'GraphVideo' )
+    {
+      if(Post4.user.edge_owner_to_timeline_media.edges[0].node.id != repositoryId)
+      {  
+        repositoryId = Post4.user.edge_owner_to_timeline_media.edges[0].node.id
+        resim4 = Post4.user.edge_owner_to_timeline_media.edges[0].node.thumbnail_src
+        video1 = Post4.user.edge_owner_to_timeline_media.edges[0].node.video_url
+        console.log(resim4)
+        videocaption1 = Post4.user.edge_owner_to_timeline_media.edges[0].node.edge_media_to_caption.edges[0].node.text
+        Downloadvideo1()
+      }
+    }
     //  if(Post5.user.edge_owner_to_timeline_media.edges[0].node.__typename == 'GraphVideo' )
     //  {
     //    if(Post5.user.edge_owner_to_timeline_media.edges[0].node.id != resim5)
     //    {
     //     resim5 = Post5.user.edge_owner_to_timeline_media.edges[0].node.id
     //     video2 = Post5.user.edge_owner_to_timeline_media.edges[0].node.video_url
-    //     videocaption2 = 'FOLLOW @canlimaclinkleri3                  #football #skill #goal'
+    //     videocaption2 = 'FOLLOW @kisahikayetv                  #football #skill #goal'
 
     //     Downloadvideo2()
     //    }
@@ -78,37 +78,37 @@ function OtoShare()
     //    {
     //     resim6 = Post6.user.edge_owner_to_timeline_media.edges[0].node.id
     //     video3 = Post6.user.edge_owner_to_timeline_media.edges[0].node.video_url
-    //     videocaption3 = 'FOLLOW @canlimaclinkleri3                  #football #skill #goal'
+    //     videocaption3 = 'FOLLOW @kisahikayetv                  #football #skill #goal'
     //     Downloadvideo3()
     //    }
     //  }
     })()
 }
-  function OtoFollow(){
-    ;(async () => {
+  // function OtoFollow(){
+  //   ;(async () => {
          
-       const Post1 = await client.getPhotosByUsername({ username: 'trtspor', first: '1', after: ''  })
-       const likes = await client.getMediaLikes({ shortcode: Post1.user.edge_owner_to_timeline_media.edges[0].node.shortcode, first: '15', after: '' })
-       likes.edges.forEach(async followItems =>
-        {
-          await client.follow({ userId: followItems.node.id })
-        })
-    })()
-  } 
-  function unFollowAllList()
-  {
-    ;(async () => {
+  //      const Post1 = await client.getPhotosByUsername({ username: 'trtspor', first: '1', after: ''  })
+  //      const likes = await client.getMediaLikes({ shortcode: Post1.user.edge_owner_to_timeline_media.edges[0].node.shortcode, first: '15', after: '' })
+  //      likes.edges.forEach(async followItems =>
+  //       {
+  //         await client.follow({ userId: followItems.node.id })
+  //       })
+  //   })()
+  // } 
+  // function unFollowAllList()
+  // {
+  //   ;(async () => {
       
-    const follows = await client.getFollowings({ userId: '43948640389',first: '49' })
+  //   const follows = await client.getFollowings({ userId: '43948640389',first: '49' })
   
   
-    follows.data.forEach(async followItems =>
-      {
-        await client.unfollow({ userId: followItems.id })
+  //   follows.data.forEach(async followItems =>
+  //     {
+  //       await client.unfollow({ userId: followItems.id })
   
-      })
-    })()
-  } 
+  //     })
+  //   })()
+  // } 
   // function LogoutAndLogin()
   // {
   //   ;(async () => {
@@ -118,17 +118,17 @@ function OtoShare()
   //     setTimeout(LoginAndStart, 70000);
   //   })()
   // } 
-  async function deleteAll()
-  {
-    const Post1 = await client.getPhotosByUsername({ username: 'canlimaclinkleri3', first: '49', after: ''  })
-    console.log(Post1.user.edge_owner_to_timeline_media.edges)
+  // async function deleteAll()
+  // {
+  //   const Post1 = await client.getPhotosByUsername({ username: 'kisahikayetv', first: '49', after: ''  })
+  //   console.log(Post1.user.edge_owner_to_timeline_media.edges)
 
-     Post1.user.edge_owner_to_timeline_media.edges.forEach(async followItems =>
-       {
-         await client.deleteMedia({ mediaId: followItems.node.id })
-         console.log(1)
-       })
-  }
+  //    Post1.user.edge_owner_to_timeline_media.edges.forEach(async followItems =>
+  //      {
+  //        await client.deleteMedia({ mediaId: followItems.node.id })
+  //        console.log(1)
+  //      })
+  // }
 function Downloadvideo1()
 {
   const http = require('https'); // or 'https' for https:// URLs
@@ -151,48 +151,48 @@ function Downloadvideo1()
 
   VideoPaylas('file1.mp4','filePhoto1.jpeg',videocaption2)
 }
-function Downloadvideo2()
-{
-  const http = require('https'); // or 'https' for https:// URLs
-  const fs = require('fs');
+// function Downloadvideo2()
+// {
+//   const http = require('https'); // or 'https' for https:// URLs
+//   const fs = require('fs');
   
-  const file = fs.createWriteStream("file2.mp4");
-  const request = http.get(video2, function(response) {
-    response.pipe(file);
+//   const file = fs.createWriteStream("file2.mp4");
+//   const request = http.get(video2, function(response) {
+//     response.pipe(file);
 
     
     
-  });
-  const filePhoto = fs.createWriteStream("filePhoto2.jpeg");
-  const request1 = http.get(resim5, function(response) {
-    response.pipe(filePhoto);
+//   });
+//   const filePhoto = fs.createWriteStream("filePhoto2.jpeg");
+//   const request1 = http.get(resim5, function(response) {
+//     response.pipe(filePhoto);
 
 
-  });
-  VideoPaylas('file2.mp4','filePhoto2.jpeg',videocaption2)
-}
-function Downloadvideo3()
-{
-  const http = require('https'); // or 'https' for https:// URLs
-  const fs = require('fs');
+//   });
+//   VideoPaylas('file2.mp4','filePhoto2.jpeg',videocaption2)
+// }
+// function Downloadvideo3()
+// {
+//   const http = require('https'); // or 'https' for https:// URLs
+//   const fs = require('fs');
   
-  const file = fs.createWriteStream("file3.mp4");
-  const request = http.get(video3, function(response) {
-    response.pipe(file);
+//   const file = fs.createWriteStream("file3.mp4");
+//   const request = http.get(video3, function(response) {
+//     response.pipe(file);
 
     
     
-  });
-  const filePhoto = fs.createWriteStream("filePhoto3.jpeg");
-  const request1 = http.get(resim6, function(response) {
-    response.pipe(filePhoto);
+//   });
+//   const filePhoto = fs.createWriteStream("filePhoto3.jpeg");
+//   const request1 = http.get(resim6, function(response) {
+//     response.pipe(filePhoto);
 
 
-  });
+//   });
 
-  VideoPaylas('file3.mp4','filePhoto3.jpeg',videocaption3)
-}
-let ID = "canlimaclinkleri3";
+//   VideoPaylas('file3.mp4','filePhoto3.jpeg',videocaption3)
+// }
+let ID = "kisahikayetv";
 let SIFRE = "5795612f";
 
 ig.state.generateDevice(ID);
@@ -214,7 +214,7 @@ function VideoPaylas(videolink,fotolink,videocaption)
           // read the file into a Buffer
           video: await readFileAsync(videoPath),
           coverImage: await readFileAsync(coverPath),
-          caption : "FOLLOW @canlimaclinkleri3                  #football #skill #goal"
+          caption : "DAHA FAZLA VIDEO İÇİN @kisahikayetv TAKİP EDİN\n #komikvideolar #çghb #güldürgüldür #güldürgüldürshow #çghb2 #skeç #mizah"
           /*
             this does also support:
             caption (string),  ----+
---
+++ test.js
@@ -0,0 +1,110 @@
+{
+    __typename: 'GraphVideo',
+    id: '2626373414740258133',
+    dimensions: { height: 695, width: 750 },
+    display_url: 'https://instagram.fist2-4.fna.fbcdn.net/v/t51.2885-15/e35/223570731_286359379906337_2776432474833084485_n.jpg?_nc_ht=instagram.fist2-4.fna.fbcdn.net&_nc_cat=105&_nc_ohc=TTwB6oBbldUAX917yVb&edm=APU89FABAAAA&ccb=7-4&oh=9aeb1ebfa2aae9d8fa23595d9cdbc2eb&oe=6102AB80&_nc_sid=86f79a',
+    display_resources: [
+      {
+        src: 'https://instagram.fist2-4.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/223570731_286359379906337_2776432474833084485_n.jpg?_nc_ht=instagram.fist2-4.fna.fbcdn.net&_nc_cat=105&_nc_ohc=TTwB6oBbldUAX917yVb&tn=g9AjMvWq5-Zv7gOY&edm=APU89FABAAAA&ccb=7-4&oh=c6b559c0bc84899f0b2299a62c6d8ad4&oe=6102D8C6&_nc_sid=86f79a',
+        config_width: 640,
+        config_height: 593
+      },
+      {
+        src: 'https://instagram.fist2-4.fna.fbcdn.net/v/t51.2885-15/e35/223570731_286359379906337_2776432474833084485_n.jpg?_nc_ht=instagram.fist2-4.fna.fbcdn.net&_nc_cat=105&_nc_ohc=TTwB6oBbldUAX917yVb&edm=APU89FABAAAA&ccb=7-4&oh=9aeb1ebfa2aae9d8fa23595d9cdbc2eb&oe=6102AB80&_nc_sid=86f79a',
+        config_width: 750,
+        config_height: 695
+      },
+      {
+        src: 'https://instagram.fist2-4.fna.fbcdn.net/v/t51.2885-15/e35/223570731_286359379906337_2776432474833084485_n.jpg?_nc_ht=instagram.fist2-4.fna.fbcdn.net&_nc_cat=105&_nc_ohc=TTwB6oBbldUAX917yVb&tn=g9AjMvWq5-Zv7gOY&edm=APU89FABAAAA&ccb=7-4&oh=36b5109ec567c948ac2ce936142ead44&oe=6102AB80&_nc_sid=86f79a',
+        config_width: 1080,
+        config_height: 1002
+      }
+    ],
+    is_video: true,
+    should_log_client_event: false,
+    tracking_token: 'eyJ2ZXJzaW9uIjo1LCJwYXlsb2FkIjp7ImlzX2FuYWx5dGljc190cmFja2VkIjp0cnVlLCJ1dWlkIjoiODAzNGE5YzNjZjA5NGE3ZDg5NzM0OTVlMmNlMzY1YTAyNjI2MzczNDE0NzQwMjU4MTMzIiwic2VydmVyX3Rva2VuIjoiMTYyNzQxMDMwMzUzMnwyNjI2MzczNDE0NzQwMjU4MTMzfDE4MTQ0NDg1Nzc4fDU5NmZmMmQzNGRlMjk1ZmNkMjY3MjgzOGIyNjI4MTU0NTNkYzc1ZDU3OTdhMDM1MTczYzU2NzNjZTY0ZDZjODMifSwic2lnbmF0dXJlIjoiIn0=',
+    edge_media_to_tagged_user: { edges: [ [Object] ] },
+    dash_info: {
+      is_dash_eligible: true,
+      video_dash_manifest: '<MPD xmlns="urn:mpeg:dash:schema:mpd:2011" minBufferTime="PT1.500S" type="static" mediaPresentationDuration="PT0H0M15.465S" maxSegmentDuration="PT0H0M5.000S" profiles="urn:mpeg:dash:profile:isoff-on-demand:2011,http://dashif.org/guidelines/dash264">\n' +
+        ' <Period duration="PT0H0M15.465S">\n' +
+        '  <AdaptationSet segmentAlignment="true" maxWidth="720" maxHeight="668" maxFrameRate="30" par="720:668" lang="und" subsegmentAlignment="true" subsegmentStartsWithSAP="1">\n' +
+        '   <Representation id="17886639401285305vd" mimeType="video/mp4" codecs="avc1.4D401F" width="720" height="668" frameRate="30" sar="1:1" startWithSAP="1" bandwidth="358390" FBQualityClass="hd" FBQualityLabel="720w" FBPlaybackResolutionMos="0:100.00,360:89.67,480:87.96,668:85.62">\n' +
+        '    <BaseURL>https://instagram.fist2-4.fna.fbcdn.net/v/t50.2886-16/223774386_3007809092832017_413036006233486102_n.mp4?_nc_ht=instagram.fist2-4.fna.fbcdn.net&amp;_nc_cat=105&amp;_nc_ohc=JCmHY5s2josAX-9CBOD&amp;edm=APU89FABAAAA&amp;ccb=7-4&amp;oh=a8eed93c1521ffc3bc0f7c0adb6c2ef6&amp;oe=61027B42&amp;_nc_sid=86f79a</BaseURL>\n' +
+        '    <SegmentBase indexRangeExact="true" indexRange="910-1001" FBFirstSegmentRange="1002-102974" FBSecondSegmentRange="102975-202473" FBPrefetchSegmentRange="1002-202473">\n' +
+        '      <Initialization range="0-909"/>\n' +
+        '    </SegmentBase>\n' +
+        '   </Representation>\n' +
+        '  <Representation id="17934442195605972v" mimeType="video/mp4" codecs="avc1.4D401F" width="340" height="316" frameRate="30" sar="1:1" startWithSAP="1" bandwidth="131151" FBQualityClass="sd" FBQualityLabel="340w" FBPlaybackResolutionMos="0:100.00,360:78.39,480:76.70,668:75.21">\n' +
+        '    <BaseURL>https://instagram.fist2-3.fna.fbcdn.net/v/t50.2886-16/224140581_176742154446193_5090067059860136168_n.mp4?_nc_ht=instagram.fist2-3.fna.fbcdn.net&amp;_nc_cat=101&amp;_nc_ohc=GIeQUiUEaHgAX9Oinqd&amp;edm=APU89FABAAAA&amp;ccb=7-4&amp;oh=52ec41fea542c6d670008098dd660725&amp;oe=6102DFE0&amp;_nc_sid=86f79a</BaseURL>\n' +
+        '    <SegmentBase indexRangeExact="true" indexRange="910-1001" FBFirstSegmentRange="1002-37241" FBSecondSegmentRange="37242-73112" FBPrefetchSegmentRange="1002-73112">\n' +
+        '      <Initialization range="0-909"/>\n' +
+        '    </SegmentBase>\n' +
+        '   </Representation>\n' +
+        '  <Representation id="17900114678049557v" mimeType="video/mp4" codecs="avc1.4D401F" width="526" height="488" frameRate="30" sar="1:1" startWithSAP="1" bandwidth="230130" FBQualityClass="sd" FBQualityLabel="526w" FBPlaybackResolutionMos="0:100.00,360:85.88,480:83.96,668:82.16">\n' +
+        '    <BaseURL>https://instagram.fist2-3.fna.fbcdn.net/v/t50.2886-16/224322228_1507379206268704_6155889829678744331_n.mp4?_nc_ht=instagram.fist2-3.fna.fbcdn.net&amp;_nc_cat=104&amp;_nc_ohc=kT6bdRcRyC4AX90nRCe&amp;edm=APU89FABAAAA&amp;ccb=7-4&amp;oh=0d84c465c1f4207a272c4a4d43b0fc9c&amp;oe=61027EB6&amp;_nc_sid=86f79a</BaseURL>\n' +
+        '    <SegmentBase indexRangeExact="true" indexRange="910-1001" FBFirstSegmentRange="1002-65519" FBSecondSegmentRange="65520-129252" FBPrefetchSegmentRange="1002-129252">\n' +
+        '      <Initialization range="0-909"/>\n' +
+        '    </SegmentBase>\n' +
+        '   </Representation>\n' +
+        '  </AdaptationSet>\n' +
+        ' <AdaptationSet segmentAlignment="true" lang="und" subsegmentAlignment="true" subsegmentStartsWithSAP="1">\n' +
+        '   <Representation id="18240685687000379ad" mimeType="audio/mp4" codecs="mp4a.40.5" audioSamplingRate="44100" startWithSAP="1" bandwidth="80451">\n' +
+        '    <AudioChannelConfiguration schemeIdUri="urn:mpeg:dash:23003:3:audio_channel_configuration:2011" value="2"/>\n' +
+        '    <BaseURL>https://instagram.fist2-4.fna.fbcdn.net/v/t50.2886-16/225399392_1161227031065131_4978465284308225561_n.mp4?_nc_ht=instagram.fist2-4.fna.fbcdn.net&amp;_nc_cat=107&amp;_nc_ohc=zyP54mGVV-cAX9po3We&amp;edm=APU89FABAAAA&amp;ccb=7-4&amp;oh=c1776587e5dee4f115b3d9c1879e342d&amp;oe=6102B096&amp;_nc_sid=86f79a</BaseURL>\n' +
+        '    <SegmentBase indexRangeExact="true" indexRange="868-995" FBFirstSegmentRange="996-19687" FBSecondSegmentRange="19688-39111" FBPrefetchSegmentRange="996-39111">\n' +
+        '      <Initialization range="0-867"/>\n' +
+        '    </SegmentBase>\n' +
+        '   </Representation>\n' +
+        '  </AdaptationSet>\n' +
+        ' </Period>\n' +
+        '</MPD>',
+      number_of_qualities: 3
+    },
+    video_url: 'https://instagram.fist2-3.fna.fbcdn.net/v/t50.2886-16/223691553_527757681703326_4124244916913302346_n.mp4?_nc_ht=instagram.fist2-3.fna.fbcdn.net&_nc_cat=106&_nc_ohc=hcH7FVOcwkMAX9fHSgV&edm=APU89FABAAAA&ccb=7-4&oe=6102AD84&oh=489507cf39fe5df7a2c2c5ede06391bb&_nc_sid=86f79a',
+    video_view_count: 14481,
+    edge_media_to_caption: { edges: [ [Object] ] },
+    shortcode: 'CRywLShH21V',
+    edge_media_to_comment: {
+      count: 6,
+      page_info: {
+        has_next_page: true,
+        end_cursor: 'QVFEcWNieWJXNTZOV2ZSc21HNjZ4dkMwdDMzWFM5VThTcENTMVVMeENieG8yX2Y4aE1JYzB2cmRXdFhZdHV4eUpSalZuQzRtZ0txVHFPVW5XQTRQd3FiUQ=='
+      },
+      edges: [ [Object], [Object], [Object], [Object] ]
+    },
+    comments_disabled: false,
+    taken_at_timestamp: 1627308392,
+    edge_media_preview_like: { count: 1020 },
+    gating_info: null,
+    media_preview: 'AComZbqEWr8Z9BVSMfKMelIYWI5x/X8Dms2dS0RrKWHPSsO/QRzE9A/zD8ev6g1cRmWPg5YHv6VQvpPM2E8kBgcjB6+n+RVRZnNaXKZ65pc0A0ny1sYmohqdmyMZxVEsUO1uCKmVlfrXO0dSZahVkHZgTzxzj/GqGosDIAOML/MmtAKUGRge44/PtWde2zMTMGVwccDgjt+NOK1uTUeljNPXipKbSY961Oc3LZ1vosyD5l+UkevqPr3HSqk9s1uwBOc8gjjj3oooe1xrsSbd6FWLcEdD6+oPUeo4qsCRlT24oooQ3uUZBtNLRRSJP//Z',
+    owner: { id: '24854348713' },
+    thumbnail_src: 'https://instagram.fist2-4.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c26.0.668.668a/s640x640/223570731_286359379906337_2776432474833084485_n.jpg?_nc_ht=instagram.fist2-4.fna.fbcdn.net&_nc_cat=105&_nc_ohc=TTwB6oBbldUAX917yVb&edm=APU89FABAAAA&ccb=7-4&oh=47692de4f06a7b2cf0891c9b9f000f2f&oe=6102B8FA&_nc_sid=86f79a',
+    thumbnail_resources: [
+      {
+        src: 'https://instagram.fist2-4.fna.fbcdn.net/v/t51.2885-15/e35/c26.0.668.668a/s150x150/223570731_286359379906337_2776432474833084485_n.jpg?_nc_ht=instagram.fist2-4.fna.fbcdn.net&_nc_cat=105&_nc_ohc=TTwB6oBbldUAX917yVb&edm=APU89FABAAAA&ccb=7-4&oh=96c658c83c0313bf4659c1890652b86b&oe=6102950E&_nc_sid=86f79a',
+        config_width: 150,
+        config_height: 150
+      },
+      {
+        src: 'https://instagram.fist2-4.fna.fbcdn.net/v/t51.2885-15/e35/c26.0.668.668a/s240x240/223570731_286359379906337_2776432474833084485_n.jpg?_nc_ht=instagram.fist2-4.fna.fbcdn.net&_nc_cat=105&_nc_ohc=TTwB6oBbldUAX917yVb&edm=APU89FABAAAA&ccb=7-4&oh=eb273e7b64c7d6f7c9acb0f8c218efb5&oe=6102A64C&_nc_sid=86f79a',
+        config_width: 240,
+        config_height: 240
+      },
+      {
+        src: 'https://instagram.fist2-4.fna.fbcdn.net/v/t51.2885-15/e35/c26.0.668.668a/s320x320/223570731_286359379906337_2776432474833084485_n.jpg?_nc_ht=instagram.fist2-4.fna.fbcdn.net&_nc_cat=105&_nc_ohc=TTwB6oBbldUAX917yVb&edm=APU89FABAAAA&ccb=7-4&oh=73bb95624f7ef6a548cf02ac70da9bba&oe=6102E236&_nc_sid=86f79a',
+        config_width: 320,
+        config_height: 320
+      },
+      {
+        src: 'https://instagram.fist2-4.fna.fbcdn.net/v/t51.2885-15/e35/c26.0.668.668a/s480x480/223570731_286359379906337_2776432474833084485_n.jpg?_nc_ht=instagram.fist2-4.fna.fbcdn.net&_nc_cat=105&_nc_ohc=TTwB6oBbldUAX917yVb&edm=APU89FABAAAA&ccb=7-4&oh=5f3576f05be8960bf6dee6a282f2ada6&oe=6102CCB3&_nc_sid=86f79a',
+        config_width: 480,
+        config_height: 480
+      },
+      {
+        src: 'https://instagram.fist2-4.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c26.0.668.668a/s640x640/223570731_286359379906337_2776432474833084485_n.jpg?_nc_ht=instagram.fist2-4.fna.fbcdn.net&_nc_cat=105&_nc_ohc=TTwB6oBbldUAX917yVb&edm=APU89FABAAAA&ccb=7-4&oh=47692de4f06a7b2cf0891c9b9f000f2f&oe=6102B8FA&_nc_sid=86f79a',
+        config_width: 640,
+        config_height: 640
+      }
+    ]
+  }
